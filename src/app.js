f594e20f3c87b9e8dbe8fbac2980bdb0803088da
Prateek Goel <goelprateek38@gmail.com>
2025-04-16 19:04:08 +0530

    setup node project and create a server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use("/test",(req,res)=>{
+    res.send("hello from the server");
+})
+
+app.listen(3000,()=>{
+    console.log("server is running correcty");
+});
